42a7a346587e534325acb4785efc72c8afd2618f
viper23 <htonnycarlos@gmail.com>
2021-10-13 23:37:35 +0200

    simple changes

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -1,5 +1,28 @@
 package main;
 
-public class Driver {
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
+public class Driver extends JFrame{
+    public Driver(){
+        super("Note Book App");
+        this.setPreferredSize(new Dimension(700,450));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setBackground(Color.DARK_GRAY);        
+        this.setLocationRelativeTo(null);
+        
+        JPanel contentPane = (JPanel) this.getContentPane();
+        
+    }
+    public static void main(String[] args) throws Exception{
+        UIManager.setLookAndFeel(new NimbusLookAndFeel());
+        Driver driver = new Driver();
+        driver.setVisible(true);
+    }
 
 }
