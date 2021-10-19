1079309e3b76d90e63e66a33a075573b351b5f32
viper23 <htonnycarlos@gmail.com>
2021-10-19 19:16:04 +0200

    fix the scroll horizontal bug

--- src/features/WindowController.java
+++ src/features/WindowController.java
@@ -9,8 +9,7 @@ public class WindowController {
     public static JScrollPane createTextArea(){
 
         JTextArea j_text_area = new JTextArea();
-        j_text_area.setLineWrap(true);
-        j_text_area.setLineWrap(true);
+        j_text_area.setLineWrap(false);
         j_text_area.setFont(Font.getFont(Constants.SEGOE_FONT));
 
         return new JScrollPane(j_text_area,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
